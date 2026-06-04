214fbabf67e2daeead8b52fa77e41979dec15486
Enzo Di Maiolo <edi-maio@3F1.42angouleme.fr>
2026-06-04 13:50:05 +0200

    aaaaaaaaaaaaaaaaaaa

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-if [ -f /entrypoint-initdb.d/init.sql.template ];
-    then
-        envsubst < ./init.sql.template > /etc/mysql/init.sql
+if [ -f /entrypoint-initdb.d/init.sql.template ]; then
+    envsubst < /entrypoint-initdb.d/init.sql.template > /etc/mysql/init.sql
 fi
 
-exec "mysqld"
+exec mysqld --init-file=/etc/mysql/init.sql
