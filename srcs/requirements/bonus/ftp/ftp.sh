13792513c33ddb04ee3711ebf899f34f3641985a
Enzo Di Maiolo <edi-maio@3F1.42angouleme.fr>
2026-06-18 21:52:14 +0200

    test ftp

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+mkdir -p /var/run/vsftpd/empty
+
 adduser --disabled-password --gecos "" $FTP_USER
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 chown -R $FTP_USER:$FTP_USER /var/www/html
+
 exec vsftpd /etc/vsftpd.conf
